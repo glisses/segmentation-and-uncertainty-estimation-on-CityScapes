aba3b896ce5eafec6d6fc821f77da3e0a332f95a
xi long <yxilong@foxmail.com>
2022-02-08 16:18:51 +0800

    data loader

---
+++ data_loader.py
@@ -0,0 +1,26 @@
+def read_file(filepath):
+    import h5py
+    f = h5py.File(filepath, "r")
+    color_codes, rgb, seg = f['color_codes'][:], f['rgb'][:], f['seg'][:]
+    return color_codes, rgb, seg
+# modify the file path when using
+color_codes_train, rgb_train, seg_train = read_file('C:/Users/a/Desktop/mit program/assignment/team project/Lab2_train_data.h5')
+color_codes_test, rgb_test, seg_test = read_file('C:/Users/a/Desktop/mit program/assignment/team project/Lab2_test_data.h5')
+
+
+
+
+print(color_codes_train[:].shape)
+print(rgb_train[:].shape)
+print(seg_train[:].shape)
+
+print(color_codes_test[:].shape)
+print(rgb_test[:].shape)
+print(seg_test[:].shape)
+
+import matplotlib.pyplot as plt
+plt.imshow(rgb_train[0][:,:,::-1])
+
+
+
+
